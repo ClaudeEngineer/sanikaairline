6bf88578eccb9bce7d40ed3fc6d63446b724139d
Chirag the Patil <nora.group4321@gmail.com>
2026-03-24 15:53:31 +0530

    remove .env from repo

--- server.js
+++ server.js
@@ -8,8 +8,16 @@ const app = express();
 
 connectDB();
 
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'http://localhost:5173'],
+//   credentials: true,
+// }));
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://skyflightsbooking.netlify.app' // 👈 your frontend URL
+  ],
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
