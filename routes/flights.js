ece96b41c4678909c2b952b9ad2fadf8a20188d5
Chirag the Patil <nora.group4321@gmail.com>
2026-03-24 17:15:22 +0530

    remove .env from repo

--- routes/flights.js
+++ routes/flights.js
@@ -4,28 +4,68 @@ const Flight  = require('../models/Flight');
 const { protect, admin } = require('../middleware/auth');
 
 // GET /api/flights/search
+// router.get('/search', async (req, res) => {
+//   try {
+//     const { from, to, date, passengers = 1, seatClass = 'economy' } = req.query;
+//     const query = { status: { $ne: 'cancelled' } };
+//     if (from) query['from.code'] = { $regex: from, $options: 'i' };
+//     if (to)   query['to.code']   = { $regex: to,   $options: 'i' };
+//     if (date) {
+//       const d = new Date(date), d2 = new Date(date);
+//       d2.setDate(d2.getDate() + 1);
+//       query.departureTime = { $gte: d, $lt: d2 };
+//     }
+//     query[`seats.${seatClass}.available`] = { $gte: parseInt(passengers) };
+//     const flights = await Flight.find(query).sort({ departureTime: 1 });
+//     // res.json(flights);
+//     res.json({
+//   success: true,
+//   count: flights.length,
+//   data: flights
+// });
+//   } catch (err) { res.status(500).json({ message: err.message }); }
+// });
 router.get('/search', async (req, res) => {
   try {
     const { from, to, date, passengers = 1, seatClass = 'economy' } = req.query;
+
     const query = { status: { $ne: 'cancelled' } };
+
     if (from) query['from.code'] = { $regex: from, $options: 'i' };
     if (to)   query['to.code']   = { $regex: to,   $options: 'i' };
+
     if (date) {
       const d = new Date(date), d2 = new Date(date);
       d2.setDate(d2.getDate() + 1);
       query.departureTime = { $gte: d, $lt: d2 };
     }
+
     query[`seats.${seatClass}.available`] = { $gte: parseInt(passengers) };
+
     const flights = await Flight.find(query).sort({ departureTime: 1 });
-    res.json(flights);
-  } catch (err) { res.status(500).json({ message: err.message }); }
+
+    res.json({
+      success: true,
+      count: flights.length,
+      data: flights
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
+
 
 // GET /api/flights/popular
 router.get('/popular', async (req, res) => {
   try {
     const flights = await Flight.find({ status: 'scheduled' }).sort({ rating: -1 }).limit(6);
-    res.json(flights);
+    // res.json(flights);
+    res.json({
+  success: true,
+  count: flights.length,
+  data: flights
+});
   } catch (err) { res.status(500).json({ message: err.message }); }
 });
 
@@ -47,7 +87,12 @@ router.post('/validate-discount', async (req, res) => {
 router.get('/', async (req, res) => {
   try {
     const flights = await Flight.find({}).sort({ departureTime: 1 });
-    res.json(flights);
+    // res.json(flights);
+    res.json({
+  success: true,
+  count: flights.length,
+  data: flights
+});
   } catch (err) { res.status(500).json({ message: err.message }); }
 });
 
